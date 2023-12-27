1b93421cca4b49b5cb62d3591bb6e5f5fb7422bf
MALAKAHED <138704447+MALAKAHED@users.noreply.github.com>
2023-12-27 14:32:25 +0200

    first assinment

---
+++ assinment_dir/assinment_1.py
@@ -0,0 +1,10 @@
+# file to code the assinment 
+My_name = "Malak"
+My_age = "19"
+My_country = "Egypt"
+
+print("\"Name:", My_name,"\""  )
+print("\"Age:", My_age,"\""  )
+print("\"Country:", My_country,"\""  )
+
+print("\"Hello, My Name Is " + My_name + " And Iam " + My_age + " Years Old and I Live in " + My_country + "\"") 
