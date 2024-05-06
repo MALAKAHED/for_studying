ff176d2edd11abf771cec5a4f655919f717105a9
MALAKAHED <lokamalek776@gmail.com>
2024-05-06 15:08:23 +0300

    adding some data

--- Founder academy/data_csv.py
+++ Founder academy/data_csv.py
@@ -1,9 +1,20 @@
 import csv
 
-headers = ['CompanyName', 'PhoneNumber', 'Website']
+headers = ['PhoneNumber', 'CompanyName', 'Website']
 
 data = [
-    ['Nile United', '+201007000849', 'http://www.nileunited.com'],
+     {'Nile United', '+201007000849', 'http://www.nileunited.com' },
+     {'Roqay', '', 'https://roqay.com/'},
+     {'wajz information technology ', '', 'www.wajz.sa'},
+     {'Digital business system', '', 'https://www.dbsmena.com'},
+     {'Boubyan digital factory ', '', 'https://boubyan.bankboubyan.com/en/boubyan-digital-factory/'},
+     {'Lavaloon', '', 'Lavaloon.com'},
+     {'Datenlotsen ', '', 'https://www.datenlotsen.de/en/'},
+     {'Awamer Elshabaka', '', 'https://aait.sa/'},
+     {'البدر للنظم الذكية', '', 'https://albadrsystems.com/ar/'},
+     {'Quarizm', '', 'https://quarizm.tech/'},
+     # Quarizm is remotly and don't have a site
+     {'serv5 web solutions', '', 'serv5.com.eg'},
 ]
 
 filename = 'data.csv'
