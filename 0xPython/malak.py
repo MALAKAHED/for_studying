b1182721f390059a14c1938ff1a548733764ae76
MALAKAHED <lokamalek776@gmail.com>
2024-10-20 21:01:10 +0300

    updates

---
+++ 0xPython/malak.py
@@ -0,0 +1,7 @@
+def add_two_numbers(a, b):
+    return a + b
+
+
+def multiply_two_numbers(a, b):
+    return a * b
+
--- 0xPython/studying.py
+++ 0xPython/studying.py
@@ -6,12 +6,11 @@ print("i love python")
 
 print(type(10))
 print(type(10.3))
-print(type([1 ,1 , 2]))
+print(type([1 ,1 , 2])) 
 print (type("hello"))
 print(type((1,2,3,5)))
 print(type({"one" : 1 , "tow" : 2}))
 print(type(2 == 2))
-
 MyVariable = "my value"
 print(MyVariable)
 MyVariable = 10
